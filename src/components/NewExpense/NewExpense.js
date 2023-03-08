8ddcd6a0d0971240301f17264c2bc6cec37f8d1c
Mami. N <mamypoco@gmail.com>
2023-03-08 11:13:53 -0800

    added Add New Expense button

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,9 @@
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+   const [isAdding, setIsAdding] = useState(false);
    //this is to pass expenseData from ExpenseForm to here. After, we pass to App.
    const onSaveExpenseDataHandler = (enteredExpenseData) => {
       const expenseData = {
@@ -9,12 +11,32 @@ const NewExpense = (props) => {
          id: Math.random().toString(),
       };
       console.log(expenseData);
+      //↓　lifting the state up
       props.onAddExpense(expenseData);
+      setIsAdding(false);
+   };
+
+   //the following is for isAdding state management.
+   const newExpenseHandler = () => {
+      setIsAdding(true);
+   };
+
+   const stopAddingHandler = () => {
+      setIsAdding(false);
    };
 
    return (
       <div className="new-expense">
-         <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
+         {!isAdding && (
+            <button onClick={newExpenseHandler}>Add New Expense</button>
+         )}
+
+         {isAdding && (
+            <ExpenseForm
+               onSaveExpenseData={onSaveExpenseDataHandler}
+               onCancel={stopAddingHandler}
+            />
+         )}
       </div>
    );
 };
